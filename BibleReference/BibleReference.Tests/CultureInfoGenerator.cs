086945f5a22fb8ae5c822d3a06965392b7423cb2
Mark Ivan Basto <markivan.developer@outlook.com>
2023-02-10 10:56:05 +0800

    add tests

---
+++ BibleReference/BibleReference.Tests/CultureInfoGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using BibleBooks;
+
+namespace BibleReference.Tests
+{
+    public class CultureInfoGenerator : IEnumerable<object?[]>
+    {
+        private readonly List<object?[]> _data = new List<object?[]>()
+        {
+            new object?[] { null },
+            new object?[] { CultureInfos.En },
+            new object?[] { CultureInfos.Fil },
+        };
+
+        public IEnumerator<object?[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
